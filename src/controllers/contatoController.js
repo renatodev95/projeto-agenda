a90bb0527b85752933233488951fd3a81f142caf
renato <renatodev95@gmail.com>
2021-06-29 08:13:08 -0300

    Editando Contatos (Update)

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -18,9 +18,7 @@ exports.register = async (req, res) => {
     }
 
     req.flash('success', 'Contato registrado com sucesso.');
-    req.session.save(() =>
-      res.redirect(`/contato/index/${contato.contato._id}`)
-    );
+    req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
     return;
   } catch (e) {
     console.log(e);
@@ -33,5 +31,27 @@ exports.editIndex = async (req, res) => {
 
   const contato = await Contato.buscaPorId(req.params.id);
   if (!contato) return res.render('404');
+
   res.render('contato', { contato });
+};
+
+exports.edit = async (req, res) => {
+  try {
+    if (!req.params.id) return res.render('404');
+    const contato = new Contato(req.body);
+    await contato.edit(req.params.id);
+
+    if (contato.errors.length > 0) {
+      req.flash('errors', contato.errors);
+      req.session.save(() => res.redirect('/contato/index/'));
+      return;
+    }
+
+    req.flash('success', 'Contato editado com sucesso.');
+    req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
+    return;
+  } catch (e) {
+    console.log(e);
+    res.render('404');
+  }
 };
